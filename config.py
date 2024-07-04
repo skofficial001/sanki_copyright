5aa2bb8d7d07dd0dc1b64275c19c26c4cb93c359
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-07-05 02:30:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = int(os.environ.get("API_ID"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7367343265:AAG8Hs-xOx5GlIooPAryLIDJO5TTKPVsjUc")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 # ------------------X------------------------------
